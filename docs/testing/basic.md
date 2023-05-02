1a7893d870cb5d7a778159521eb51858bf252515
Shaddow798 <samorange370@gmail.com>
2023-05-02 12:45:19 +1200

    lesson over added some routes

---
+++ docs/testing/basic.md
@@ -0,0 +1,10 @@
+This Document consits of the very basic tests that the website needs to pass in order to function.
+
+
+| Test                             | Working? | Date        | Notes |
+|----------------------------------|----------|-------------|-------|
+| Flask Server Launches            | ✅        | 2023/05/02  |       |
+| Returns homepage on '/' route    | ✅        | 2023/05/02  |       |
+| Base Page Template               | ✅        | 2023/05/02  |       |
+| Navbar                           | ✅        | 2023/05/02  |       |
+| Very Basic css                   | ✅        | 2023/05/02  |       |
--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
+import wheel.bdist_wheel
 from flask import Flask, render_template
-
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,6 +10,17 @@ def index():
     return render_template("index.html", title="Homepage")
 
 
+@app.route('/about')
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/all_machines")
+def machines():
+    # conn = sqlite3.connect('project.db')
+    return render_template("all_machines.html", title="Machines")
+
+
 # Start a basic inbuilt flask web server, should not use in a production but none of this is made for production.
 if __name__ == '__main__':
-    app.run(debug=True,port=6969)
+    app.run(debug=True,port=8080)
+
