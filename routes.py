da707b15ab2081089f60e55376bd292f85305da7
Shaddow798 <samorange370@gmail.com>
2023-05-16 21:54:13 +1200

    work on submition

--- routes.py
+++ routes.py
@@ -1,8 +1,16 @@
 import wheel.bdist_wheel
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, flash
 import sqlite3
+import os
 
 app = Flask(__name__)
+
+
+def get_db_connection():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "project.db")
+    conn = sqlite3.connect(db_path)
+    return conn
 
 
 @app.route('/')
@@ -33,11 +41,24 @@ def machine(id):
     return render_template('machine.html', title="Machine", machine=data,)
 
 
-@app.route("/machine/submit")
+# At least part of this block of code is sourced from another one of my projects at home.
+@app.route("/machine/submit", methods=('GET', 'POST'))
 def machine_submit():
-    conn = sqlite3.connect('project.db')
-    cur = conn.cursor()
+    if request.method == 'POST':
+        name = request.form["machine"]
+        description = request.form["machine_description"]
 
+        if not name:
+            flash('Title is required!')
+        elif not description:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO Machine (1, 2) VALUES (?, ?)',
+                         (name, description))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
     return render_template('submission.html', title="Submit A Machine")
 
 
@@ -66,5 +87,5 @@ def error(e):
 
 # Start a basic inbuilt flask web server, should not use in a production but none of this is made for production.
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=True, port=8080)
 
